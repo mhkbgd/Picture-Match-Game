eb410f59f5f1e372d0e31f371e0beaf5cd24bc97
LAPTOP-4L8OU9T6\Mahmud <mhkbgd@Gmail.com>
2017-09-19 21:21:28 +0800

    First commit of picture match game

---
+++ Assets/Scripts/MenuBehavior.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.IO;
+using System.Text;
+
+
+public class MenuBehavior : MonoBehaviour {
+    public Dropdown patientDropdown;
+
+    public InputField date;
+    public string currentPatient;
+    public void mainMenuTrigger(int i) {
+        switch (i) {
+            default:
+            case (0):
+                selection();
+                Savecsv();
+                break;
+
+            case (1):
+                UnityEngine.Application.Quit();
+                break;
+
+        }
+       
+    }
+    
+    
+
+    public void selection()
+    {
+        switch (patientDropdown.value)
+        {
+            default:
+                break;
+            case 0:
+                currentPatient = "Mahmud";
+                SceneManager.LoadScene("level1");
+                break;
+            case 1:
+                SceneManager.LoadScene("Shahin");
+                break;
+        }
+    }
+    void Savecsv()
+    {
+        string filePath = @"/"+currentPatient+"-"+date.text+ ".csv";
+        string delimiter = ",";
+
+        string[][] output = new string[][]{
+             new string[]{"Col 1 Row 1", "Col 2 Row 1", "Col 3 Row 1"},
+             new string[]{"Col1 Row 2", "Col2 Row 2", "Col3 Row 2"}
+         };
+        int length = output.GetLength(0);
+        StringBuilder sb = new StringBuilder();
+        for (int index = 0; index < length; index++)
+            sb.AppendLine(string.Join(delimiter, output[index]));
+
+        File.WriteAllText(filePath, sb.ToString());
+    }
+}
---
+++ Assets/Scripts/level.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class level : MonoBehaviour
+{
+    public void quit() {
+        Application.Quit();
+
+    }
+
+  
+    
+
+    public void level1(string result)
+    {
+        switch (result)
+        {
+            default:
+            case "correct":
+
+                SceneManager.LoadScene("level2");
+                break;
+
+            case "wrong":
+                SceneManager.LoadScene("level2");
+                break;
+
+            
+        }
+
+    }
+
+
+    public void level2(string result)
+    {
+        switch (result)
+        {
+            default:
+            case "correct":
+
+                SceneManager.LoadScene("level3");
+                break;
+
+            case "wrong":
+                SceneManager.LoadScene("level3");
+                break;
+
+
+        }
+
+    }
+
+   
+}
